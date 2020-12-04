ca344d7de0aa2892a3a3271c561796bca618f3a5
Raul <rquispeto@unsa.edu.pe>
2020-12-03 20:10:45 -0500

    feat: created app colors

--- lib/src/constants/app_colors.dart
+++ lib/src/constants/app_colors.dart
@@ -9,4 +9,10 @@ class AppColors {
   static const Color ELECTROMACNETIC = Color.fromRGBO(47, 54, 64, 1);
   static const Color Hexadecimal = const Color(0xFFB74093);
   static const Color ORANGE = Colors.orange;
+  static const Color PRIMARY = const Color(0xFF028F96);
+  static const Color SECONDARY = const Color(0xFFE8673C);
+  static const Color PRIMARY_DARK = const Color(0xFF1662C9);
+  static const Color PRIMARY_LIGHT = const Color(0xFF68A7FC);
+  static const Color WHITE = const Color(0xFFFFFFFF);
+  static const Color BLACK = const Color(0xFF000000);
 }

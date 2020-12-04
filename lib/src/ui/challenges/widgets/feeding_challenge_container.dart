e922983f1491b463203dac4074408b5aad2bbc52
Raul <rquispeto@unsa.edu.pe>
2020-12-03 22:20:31 -0500

    feat: complete feeding chanllenges containers

--- lib/src/ui/challenges/widgets/feeding_challenge_container.dart
+++ lib/src/ui/challenges/widgets/feeding_challenge_container.dart
@@ -1,4 +1,6 @@
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:health_body_checking/src/constants/app_colors.dart';
 
@@ -11,26 +13,34 @@ class FeedingChallengeContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(20),
       width: double.infinity,
       height: 200,
-      decoration: BoxDecoration(color: AppColors.WHITE, borderRadius: BorderRadius.circular(10), boxShadow: [BoxShadow(color: Colors.black12, offset: Offset(0.0, 15.0), blurRadius: 15.0), BoxShadow(color: Colors.black12, offset: Offset(0.0, -10.0), blurRadius: 10.0)]),
+      decoration: BoxDecoration(color: AppColors.WHITE, borderRadius: BorderRadius.circular(10), 
+      boxShadow: [BoxShadow(color: Colors.black12, offset: Offset(0.0, 15.0), blurRadius: 15.0)]),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(contentText),
+          Container(
+            width: 200,
+            child: Text(contentText,style: TextStyle(fontSize: 22),textAlign: TextAlign.center,)),
+          Expanded(child: SizedBox()),
           Column(
             children: [
+              SizedBox(height: 10,),
               Image.asset(imagePath,width: 100,height: 100,),
+              SizedBox(height: 10,),
               InkWell(
                 onTap: inputFunction,
                 child: Container(
-                  width: 140,
-                  height: 50,
+                  width: 120,
+                  height: 40,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: AppColors.SECONDARY
                   ),
                   child: Center(
-                    child: Text('VER RETOS'),
+                    child: Text('VER RETOS',style: TextStyle(color: AppColors.WHITE),),
                   ),
                 ),
               )

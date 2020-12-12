9b866155dd472c422fb397be10b2af17dbce7b55
MBlev <mb.lev17@gmail.com>
2020-12-12 18:41:03 -0500

    Actualizando monitor

--- lib/src/ui/monitor/monitor_screen.dart
+++ lib/src/ui/monitor/monitor_screen.dart
@@ -1,104 +1,96 @@
-// import 'package:flutter/material.dart';
-// import 'package:health_body_checking/src/constants/app_colors.dart';
-// import 'package:health_body_checking/src/models/sensor_model.dart';
-// import 'package:health_body_checking/src/ui/monitor/widgets/sensor_card.dart';
-// import '../../core/routes/routes.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:health_body_checking/src/constants/app_colors.dart';
+import 'package:health_body_checking/src/models/sensor_model.dart';
+import 'package:health_body_checking/src/ui/monitor/widgets/sensor_card.dart';
+import '../../core/routes/routes.dart';
 
-// class MonitorScreen extends StatefulWidget {
-//   MonitorScreen({Key key}) : super(key: key);
+class MonitorScreen extends StatefulWidget {
+  MonitorScreen({Key key}) : super(key: key);
 
-//   @override
-//   _MonitorScreenState createState() => _MonitorScreenState();
-// }
+  @override
+  _MonitorScreenState createState() => _MonitorScreenState();
+}
 
-// class _MonitorScreenState extends State<MonitorScreen> {
-//   final _scaffoldKey = GlobalKey<ScaffoldState>();
+class _MonitorScreenState extends State<MonitorScreen> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
-//   static const _kFontFam = 'Health';
-//   static const _kFontPkg = null;
+  static const _kFontFam = 'Health';
+  static const _kFontPkg = null;
 
-//   List<SensorModel> items = [
-//     SensorModel(value: 35.6, time: "today"),
-//     SensorModel(value: 35.6, time: "today"),
-//     SensorModel(value: 35.6, time: "today")
-//   ];
-//   static const List<IconData> icons = [
-//     IconData(0xf21e, fontFamily: _kFontFam, fontPackage: _kFontPkg),
-//     IconData(0xf496, fontFamily: _kFontFam, fontPackage: _kFontPkg),
-//     IconData(0xf2c9, fontFamily: _kFontFam, fontPackage: _kFontPkg)
-//   ];
-//   List<String> names = [
-//     "Latidos por minuto",
-//     "Índice de masa corporal",
-//     "Temperatura"
-//   ];
+  List<SensorModel> items = [
+    SensorModel(value: 35.6, time: Timestamp(10,5)),
+    SensorModel(value: 35.6, time: Timestamp(10,5)),
+    SensorModel(value: 35.6, time: Timestamp(10,5))
+  ];
+  static const List<IconData> icons = [
+    IconData(0xf21e, fontFamily: _kFontFam, fontPackage: _kFontPkg),
+    IconData(0xf496, fontFamily: _kFontFam, fontPackage: _kFontPkg),
+    IconData(0xf2c9, fontFamily: _kFontFam, fontPackage: _kFontPkg)
+  ];
+  List<String> names = [
+    "Latidos por minuto",
+    "Índice de masa corporal",
+    "Temperatura"
+  ];
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return DefaultTabController(
-//       length: 2,
-//       child: Scaffold(
-//         key: _scaffoldKey,
-//         appBar: AppBar(
-//           backgroundColor: Colors.transparent,
-//           elevation: 0.0,
-//           title: _tabBarHeader()
-//         ),
-//         body: TabBarView(
-//           children: [_monitor(), _riskMap()],
-//         ),
-//       )
-//     );
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          title: _tabBarHeader()
+        ),
+        body: TabBarView(
+          children: [_monitor(), _riskMap()],
+        ),
+      )
+    );
+  }
 
-//   Widget _tabBarHeader() {
-//     return TabBar(
-//       unselectedLabelColor: AppColors.PRIMARY_DARK,
-//       indicatorSize: TabBarIndicatorSize.label,
-//       indicator: BoxDecoration(
-//         borderRadius: BorderRadius.circular(50),
-//         color: AppColors.PRIMARY_DARK),
-//       tabs: [_tabBarHeaderTextContainer(text: 'Monitor'), _tabBarHeaderTextContainer(text: 'Mapa de riesgo')]
-//     );
-//   }
+  Widget _tabBarHeader() {
+    return TabBar(
+      unselectedLabelColor: AppColors.PRIMARY_DARK,
+      indicatorSize: TabBarIndicatorSize.label,
+      indicator: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: AppColors.PRIMARY_DARK),
+      tabs: [_tabBarHeaderTextContainer(text: 'Monitor'), _tabBarHeaderTextContainer(text: 'Mapa de riesgo')]
+    );
+  }
 
-//   Widget _tabBarHeaderTextContainer({String text}) {
-//     return Container(
-//       padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-//       decoration: BoxDecoration(
-//         borderRadius: BorderRadius.circular(50),
-//         border: Border.all(color: AppColors.PRIMARY_DARK, width: 1)),
-//       child: Align(
-//         alignment: Alignment.center,
-//         child: Text(text),
-//       ),
-//     );
-//   }
+  Widget _tabBarHeaderTextContainer({String text}) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        border: Border.all(color: AppColors.PRIMARY_DARK, width: 1)),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(text),
+      ),
+    );
+  }
 
-//   Widget _monitor() {
-//     return ListView.builder(
-//       itemCount: items.length,
-//       itemBuilder: (context, index) {
-//         return SensorCard(icon: icons[index], name: names[index], sensor: items[index],);
-//       },
-//     );
-//   }
+  Widget _monitor() {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return SensorCard(icon: icons[index], name: names[index], sensor: items[index],);
+      },
+    );
+  }
 
-//   Widget _riskMap() {
-//     return Column(
-//     return Scaffold(
-//       body: Column(
-//         children: [
-//           MaterialButton(
-//             child: Text('Click!'),
-//             onPressed: () {
-//             Navigator.of(context).pushNamed(Routes.data_visualization);
-//           }),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  Widget _riskMap() {
+    return Container();
+  }
+}
+
+/*
 import 'package:flutter/material.dart';
 import 'package:health_body_checking/src/core/routes/routes.dart';
 
@@ -126,3 +118,4 @@ class _MonitorScreenState extends State<MonitorScreen> {
     );
   }
 }
+*/
--- lib/src/ui/monitor/widgets/sensor_card.dart
+++ lib/src/ui/monitor/widgets/sensor_card.dart
@@ -1,56 +1,56 @@
-// import 'dart:ui';
+import 'dart:ui';
 
-// import 'package:flutter/material.dart';
-// import 'package:health_body_checking/src/constants/app_colors.dart';
-// import 'package:health_body_checking/src/models/sensor_model.dart';
+import 'package:flutter/material.dart';
+import 'package:health_body_checking/src/constants/app_colors.dart';
+import 'package:health_body_checking/src/models/sensor_model.dart';
 
-// class SensorCard extends StatefulWidget {
+class SensorCard extends StatefulWidget {
 
-//   final SensorModel sensor;
-//   final String name;
-//   final IconData icon;
-//   final VoidCallback inputFunction;
-//   SensorCard({Key key, this.sensor, this.name, this.icon, this.inputFunction}) : super(key: key);
+  final SensorModel sensor;
+  final String name;
+  final IconData icon;
+  final VoidCallback inputFunction;
+  SensorCard({Key key, this.sensor, this.name, this.icon, this.inputFunction}) : super(key: key);
 
-//   @override State createState() => SensorCardState();
-// }
+  @override State createState() => SensorCardState();
+}
 
-// class SensorCardState extends State<SensorCard> {
+class SensorCardState extends State<SensorCard> {
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//       margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-//       child: Container(
-//         padding: EdgeInsets.all(20),
-//         width: double.infinity,
-//         height: 200,
-//         decoration: BoxDecoration(color: AppColors.WHITE, borderRadius: BorderRadius.circular(10),
-//           boxShadow: [BoxShadow(color: Colors.black12, offset: Offset(0.0, 15.0), blurRadius: 15.0)]),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Container(
-//               width: 150,
-//               padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.end,
-//                 children: [
-//                   Icon(widget.icon),
-//                   Text(widget.sensor.value.toString(), style: TextStyle(fontSize: 25),),
-//                   Text(widget.name, textAlign: TextAlign.center,),
-//                 ],
-//               )
-//             ),
-//             Expanded(
-//               child: Container(
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        width: double.infinity,
+        height: 200,
+        decoration: BoxDecoration(color: AppColors.WHITE, borderRadius: BorderRadius.circular(10),
+          boxShadow: [BoxShadow(color: Colors.black12, offset: Offset(0.0, 15.0), blurRadius: 15.0)]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(widget.icon),
+                  Text(widget.sensor.value.toString(), style: TextStyle(fontSize: 25),),
+                  Text(widget.name, textAlign: TextAlign.center,),
+                ],
+              )
+            ),
+            Expanded(
+              child: Container(
 
-//               )
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 

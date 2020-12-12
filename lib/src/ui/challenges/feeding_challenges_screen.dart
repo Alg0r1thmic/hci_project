20458279eb6481bd082a1fa5216f9e181597805d
MBlev <mb.lev17@gmail.com>
2020-12-12 18:40:24 -0500

    Desafíos creado por lista

--- lib/src/ui/challenges/feeding_challenges_screen.dart
+++ lib/src/ui/challenges/feeding_challenges_screen.dart
@@ -3,6 +3,7 @@ import 'package:health_body_checking/src/constants/app_colors.dart';
 import 'package:health_body_checking/src/constants/fake_water_days.dart';
 import 'package:health_body_checking/src/core/routes/routes.dart';
 import 'package:health_body_checking/src/ui/challenges/widgets/start_challenge_container.dart';
+import 'package:health_body_checking/src/models/challenge_model.dart';
 
 class FeedingChallengesScreen extends StatefulWidget {
   FeedingChallengesScreen({Key key}) : super(key: key);
@@ -12,6 +13,13 @@ class FeedingChallengesScreen extends StatefulWidget {
 }
 
 class FeedingChallengesScreenState extends State<FeedingChallengesScreen> with WidgetsBindingObserver {
+
+  List<ChallengeModel> challenges = [
+    ChallengeModel(amount: "1 vaso diario", duration: "1 semana", enable: true, complete: false),
+    ChallengeModel(amount: "2 vasos diarios", duration: "1 semana", enable: false, complete: false),
+    ChallengeModel(amount: "3 vasos diarios", duration: "1 semana", enable: false, complete: false),
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -40,69 +48,34 @@ class FeedingChallengesScreenState extends State<FeedingChallengesScreen> with W
         child: Icon(Icons.arrow_back),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          Center(
-              child: Image.asset(
-            'assets/images/glass-of-water.png',
-            width: 80,
-            height: 80,
-          )),
-          SizedBox(
-            height: 10,
-          ),
-          Expanded(
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                (FakeWaterDays().currentDay < 6)
-                    ? StartChallengeContainer(
-                        amountText: '1 Vaso diario',
-                        durationText: '1 Semana',
-                        imagePath: 'assets/images/unlock.png',
-                        inputFunction: () {
-                          Navigator.pushNamed(context, Routes.current_challenge);
-                        },
-                      )
-                    : StartChallengeContainer(
-                        amountText: '1 Vaso diario',
-                        durationText: '1 Semana',
-                        imagePath: 'assets/images/check_1.png',
-                        complete: true,
-                        inputFunction: () {
-                          Navigator.pushNamed(context, Routes.current_challenge);
-                        },
-                      ),
-                (FakeWaterDays().currentDay == 6)
-                    ? StartChallengeContainer(
-                        amountText: '2 Vaso diario',
-                        imagePath: 'assets/images/unlock.png',
-                        durationText: '1 Semana',
-                      )
-                    : Opacity(
-                        opacity: 0.5,
-                        child: StartChallengeContainer(
-                          amountText: '2 Vaso diario',
-                          imagePath: 'assets/images/padlock.png',
-                          durationText: '1 Semana',
-                        ),
-                      ),
-                Opacity(
-                  opacity: 0.5,
-                  child: StartChallengeContainer(
-                    amountText: '3 Vaso diario',
-                    imagePath: 'assets/images/padlock.png',
-                    durationText: '1 Semana',
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 15),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView. builder(
+                  itemCount: challenges.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return _getChallenge(challenges[index].amount, challenges[index].duration, challenges[index].enable);
+                  }
+              )
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _getChallenge(String amount, String duration, bool enable) {
+    return StartChallengeContainer(
+        amountText: amount,
+        durationText: duration,
+        enable: enable,
+        complete: (FakeWaterDays().currentDay < 6) ? false : true,
+        inputFunction: () {
+          Navigator.pushNamed(context, Routes.current_challenge);
+        }
     );
   }
 }

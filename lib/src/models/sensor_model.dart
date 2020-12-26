b3e5bbed5def502c3b328655027872be5505a6a2
MBlev <mb.lev17@gmail.com>
2020-12-26 12:42:35 -0500

    Hardcoded lists

--- lib/src/models/sensor_model.dart
+++ lib/src/models/sensor_model.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 
 SensorModel sensorModelFromJson(String str) => SensorModel.fromJson(json.decode(str));
 
@@ -31,3 +32,26 @@ class SensorModel {
         "enabled": enabled,
       };
 }
+
+class Values {
+  Values({this.time, this.value});
+  double value;
+  DateTime time;
+}
+
+class Sensor {
+  Sensor({this.id, this.name, this.enabled, this.values, this.icon});
+  int id;
+  String name;
+  bool enabled=false;
+  IconData icon;
+  List<Values> values;
+}
+
+List<Sensor> sensors =  [
+  Sensor(id: 1, name: "Indice de masa corporal", enabled: true, values: <Values>[], icon: IconData(0xf496, fontFamily: "Health", fontPackage: null)),
+  Sensor(id: 2, name: "Latidos por minuto", enabled: true, values: <Values>[], icon: IconData(0xf21e, fontFamily: "Health", fontPackage: null)),
+  Sensor(id: 3, name: "Temperatura", enabled: true, values: <Values>[], icon: IconData(0xf2c9, fontFamily: "Health", fontPackage: null)),
+  Sensor(id: 4, name: "Saturacion de oxigeno", enabled: true, values: <Values>[], icon: IconData(0xf604, fontFamily: 'Lungs', fontPackage: null)),
+];
+
